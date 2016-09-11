5fe22b213c56441bcec6db1267f48f5047e6e8e4
KoltPenny <kolt.penny@gmail.com>
2016-09-11 01:50:12 -0500

    Removed special variables and state-check function

--- automata.lisp
+++ automata.lisp
@@ -1,21 +1,13 @@
-(defun latom (e) (car (last e)))
-
-(defparameter *curr-st* nil)
-
-(defun state-check (q sigma)
-  "executes the relation of pertenence and returns a state"
-  (if (assoc sigma (rest (assoc *curr-st* q)))
-      (setf *curr-st* (latom (assoc sigma (rest (assoc *curr-st* q)))))
-      'nr))
-
 (defun automata (q str)
   "Checks if a string belongs to an alphabet and if it validates a terminal state"
-  (setf *curr-st* (caar q))
-  (let ((states nil) (strlist nil))
+  (let ((states nil) (strlist nil) (curr-st (caar q)))
     (setf strlist (map 'list #'(lambda (x) x) str))
-    (setf states (mapcar #'(lambda (x) (state-check q x)) strlist))
+    (setf states (mapcar #'
+		  (lambda (sigma)
+		    (if (assoc sigma (rest (assoc curr-st q)))
+			(setf curr-st (car(last(assoc sigma (rest (assoc curr-st q))))))
+			'nr)) strlist))
     (if (not (find 'nr states))
-	(if (find 't (assoc *curr-st* q))
-	    (format t "SUCCESS")
-	    (format t "FAILED"))
+	(if (find 't (assoc curr-st q))
+	    (format t "SUCCESS")(format t "FAILED"))
 	(format t "NOT RECOGNISED"))))
