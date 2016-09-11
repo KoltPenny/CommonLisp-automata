8ca845afb3ad176032bb3b91bbd45985294e8d4c
KoltPenny <kolt.penny@gmail.com>
2016-09-11 01:07:09 -0500

    Redefined variables and functions

--- automata.lisp
+++ automata.lisp
@@ -1,23 +1,21 @@
-(defmacro latom (e) `(car (last ,e)))
+(defun latom (e) (car (last e)))
 
-(defparameter curr-st nil)
-(defparameter strlist nil)
+(defparameter *curr-st* nil)
 
 (defun state-check (q sigma)
   "executes the relation of pertenence and returns a state"
-  (if (assoc sigma (rest (assoc curr-st q)))
-      (setf curr-st (latom (assoc sigma (rest (assoc curr-st q)))))
+  (if (assoc sigma (rest (assoc *curr-st* q)))
+      (setf *curr-st* (latom (assoc sigma (rest (assoc *curr-st* q)))))
       'nr))
 
 (defun automata (q str)
   "Checks if a string belongs to an alphabet and if it validates a terminal state"
-  (setf curr-st (caar q))
-  (setf strlist nil)
-  (let ((states nil))
+  (setf *curr-st* (caar q))
+  (let ((states nil) (strlist nil))
     (setf strlist (map 'list #'(lambda (x) x) str))
     (setf states (mapcar #'(lambda (x) (state-check q x)) strlist))
     (if (not (find 'nr states))
-	(if (find 't (assoc curr-st q))
+	(if (find 't (assoc *curr-st* q))
 	    (format t "SUCCESS")
 	    (format t "FAILED"))
 	(format t "NOT RECOGNISED"))))
