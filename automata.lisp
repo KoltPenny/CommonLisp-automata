531f37384ad521d0dc36ccb7d7faf4d4aff55ee4
KoltPenny <kolt.penny@gmail.com>
2016-09-11 23:02:14 -0500

    Update automata.lisp

--- automata.lisp
+++ automata.lisp
@@ -1,7 +1,10 @@
 (defun automata (q str &aux (state (caar q)) (l-str (coerce str 'list)))
   "Check if a string belongs to an alphabet and if it validates a terminal state."
-  (format
-   t(cond((find 'nr (mapcar #'(lambda(s)(if(assoc s (rest (assoc state q)))
-					   (setf state (car(last(assoc s (rest(assoc state q))))))
-					   'nr)) l-str)) "NOT RECOGNISED")
-	 ((find 't (assoc state q)) "SUCCESS")(t "FAILED"))))
+  (format t
+	  (cond
+	    ((find 'nr (mapcar #'(lambda (s)
+				   (if(assoc s (rest (assoc state q)))
+				      (setf state (car(last(assoc s (rest(assoc state q))))))
+				      'nr)) l-str)) "NOT RECOGNISED")
+	    ((find 't (assoc state q)) "SUCCESS")
+	    (t "FAILED"))))
