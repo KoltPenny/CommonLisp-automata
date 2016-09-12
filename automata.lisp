04f16afc769fcca7b812bf6f90118a2e81f0387d
KoltPenny <kolt.penny@gmail.com>
2016-09-11 23:09:05 -0500

    Removed unnecessary variables

--- automata.lisp
+++ automata.lisp
@@ -1,10 +1,10 @@
-(defun automata (q str &aux (state (caar q)) (l-str (coerce str 'list)))
+(defun automata (q str &aux (state (caar q)))
   "Check if a string belongs to an alphabet and if it validates a terminal state."
   (format t
 	  (cond
-	    ((find 'nr (mapcar #'(lambda (s)
-				   (if(assoc s (rest (assoc state q)))
-				      (setf state (car(last(assoc s (rest(assoc state q))))))
-				      'nr)) l-str)) "NOT RECOGNISED")
+	    ((find 'nr (map 'list #'(lambda (s)
+				      (if(assoc s (rest (assoc state q)))
+					 (setf state (car(last(assoc s (rest(assoc state q))))))
+					 'nr)) str)) "NOT RECOGNISED")
 	    ((find 't (assoc state q)) "SUCCESS")
 	    (t "FAILED"))))
